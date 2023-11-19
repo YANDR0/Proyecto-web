8fd43253d79b7c9ffb7abcf2be54c23b457cf69a
Yael <yael.rodriguez@iteso.mx>
2023-11-19 13:58:17 -0600

    Mondongo

--- server.js
+++ server.js
@@ -1,14 +1,87 @@
-const express = require('express');     //Llamada a express para hacer uso de este
-const router = require('./app/controllers/router');     //Conexión al archivo router
-const puerto = 3000;      //Puerto a conectarse
-const app = express();      //Forma de inicializar los middlewares
+const mongoose = require("mongoose");
+//const cors = require("cors");
+const express = require('express');
+const router = require('./app/controllers/router');
+const puerto = 3000;
+const app = express();
+
 
 app.use(express.json());
-app.use(router);    //Mandar router
+app.use(router);
 
 app.use(express.static('app'));
 app.use('/views', express.static('views'));
 
+
+//MONGO CONECTION
+let mongoConnection = "mongodb+srv://admin:Parangaricutirimicuaro01@myapp.j2ikbis.mongodb.net/MyApp";
+let db = mongoose.connection;
+
+db.on('connecting', () => {
+        console.log('Conectando a la base...');
+        console.log(mongoose.connection.readyState);
+});
+    
+db.on('connected', () => {
+        console.log('¡Conectado exitosamente!');
+        console.log(mongoose.connection.readyState);
+});
+
+mongoose.connect(mongoConnection, {useNewUrlParser: true});
+
+
+//SCHEMAS
+let userSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true
+    },
+    correo: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    pass: {
+        type: String,
+        required: true
+    }
+});
+
+let productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: String,
+        required: true
+    },
+    stock: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    }
+});
+
+let User = mongoose.model("users", userSchema);
+let Product = mongoose.model("products", userSchema);
+
+
+//CONECTION TO PORT
 app.listen(puerto, () => {
     console.log("Página del proyecto iniciada en el puerto: " + puerto);
 });

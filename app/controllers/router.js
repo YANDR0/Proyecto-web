4ccf91b57a7199b455dd696d1ba0d53867d90fc0
Yael <yael.rodriguez@iteso.mx>
2023-11-19 11:58:24 -0600

    El flojo trabaja doble

--- app/controllers/router.js
+++ app/controllers/router.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
-//const productRouter = require('../routes/products')
+const productRouter = require('../routes/products')
 //const adminProductRouter = require('../routes/admin_products')
 const router = express.Router();
 
-//router.use('/products', productRouter);
+router.use('/products', productRouter);
 //router.use('/admin/products', validateAdmin, adminProductRouter);
 
 function validateAdmin(req, res, next){

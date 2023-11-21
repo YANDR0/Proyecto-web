fa3e75d42898130ad3b3462d92f6b474b8ffd39f
Yael <yael.rodriguez@iteso.mx>
2023-11-21 10:22:02 -0600

    Luego de la clase evito que salgan repetidos

--- app/scripts/home.js
+++ app/scripts/home.js
@@ -1,6 +1,7 @@
-sessionStorage.clear();
 
-let inventory
+var inventory = [];
+var carrito = [];
+var curr;
 let xhr = new XMLHttpRequest();
 xhr.open('GET', 'http://localhost:3000/products');
 xhr.send();
@@ -17,7 +18,7 @@ function productsToDisplay(itemsToDisplay) {
     let html = "";
     for (const e of itemsToDisplay) {
         html += `
-            <button class="flex flex-col rounded-lg bg-slate-700 md:flex-row chistoso" uuid="${e._id}" onclick="focusProduct('${e._id}','${e.name}', '${e.description}', '${e.price}', '${e.stock}', '${e.image}')">
+            <div class="flex flex-col rounded-lg bg-slate-700 md:flex-row chistoso" uuid="${e._id}" onclick="focusProduct('${e._id}')">
                 <img class="w-1/4 h-auto flex-1 rounded-t-lg object-cover md:!rounded-none md:!rounded-l-lg"
                 src="${e.image}">
                 <div class="w-3/4 justify-start p-6 text-slate-300">
@@ -28,7 +29,7 @@ function productsToDisplay(itemsToDisplay) {
                         Precio: ${e.price}
                     </p>
                 </div>
-            </button>
+            </div>
         `
     }
     document.getElementById("showProducts").innerHTML = html;
@@ -49,7 +50,7 @@ function productsToDisplay(itemsToDisplay) {
                         <div class="flex justify-center items-center rounded-lg shadow-sm">
                             <button
                                 class="py-3 px-4 gap-x-2 text-sm font-semibold rounded-md border border-transparent bg-blue-500 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                                onclick="focusProductsFunctions('${itemsToDisplay[0].id}', '${itemsToDisplay[0].name}', '${itemsToDisplay[0].description}', '${itemsToDisplay[0].price}', '${itemsToDisplay[0].stock}', '${itemsToDisplay[0].image}')">
+                                onclick="focusProductsFunctions('${itemsToDisplay[0]._id}', '${itemsToDisplay[0].name}', '${itemsToDisplay[0].description}', '${itemsToDisplay[0].price}', '${itemsToDisplay[0].stock}', '${itemsToDisplay[0].image}')">
                                 Comprar
                             </button>
                         </div>
@@ -58,25 +59,26 @@ function productsToDisplay(itemsToDisplay) {
     `
 }
 
-function focusProduct(id, name, description, price, stock, image) {
+function focusProduct(uuid) {
+    let p = inventory.find((p) => p._id == uuid);
     document.getElementById('focusedProduct').innerHTML = `
         <div class="rounded overflow-hidden bg-slate-600">
             <img class="w-full"
-            src="${image}"
+            src="${p.image}"
             alt="Sunset in the mountains">
             <div class="px-6 py-4 flex flex-col text-slate-300">
-                <div class="font-bold text-xl mb-2">${name}</div>
+                <div class="font-bold text-xl mb-2">${p.name}</div>
                     <p class=" text-base">
-                        ${description}
+                        ${p.description}
                     </p>
                     <p class=" text-base">
-                        Precio: ${price}
+                        Precio: ${p.price}
                     </p>
                     <br>
                     <div class="flex justify-center items-center rounded-lg shadow-sm">
                     <button
                         class="py-3 px-4 gap-x-2 text-sm font-semibold rounded-md border border-transparent bg-blue-500 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                        onclick="focusProductsFunctions('${id}', '${name}', '${description}', '${price}', '${stock}', '${image}')">
+                        onclick="focusProductsFunctions('${p._id}')">
                         Comprar
                     </button>
                 </div>
@@ -89,8 +91,8 @@ function focusProduct(id, name, description, price, stock, image) {
 // Función para inicializar el carrito
 function inicializarCarrito() {
     if (sessionStorage.getItem('carrito') === null) {
-        const carrito = [];
-        sessionStorage.setItem('carrito', JSON.stringify(carrito));
+        const start = [];
+        sessionStorage.setItem('carrito', JSON.stringify(start));
     }
 }
 
@@ -105,13 +107,8 @@ function vaciarCarrito() {
     sessionStorage.removeItem('carrito');
 }
 
-function setSessionData(id, name, description, price, stock, imageUrl) {
-    sessionStorage.setItem('id', id);
-    sessionStorage.setItem('name', name);
-    sessionStorage.setItem('description', description);
-    sessionStorage.setItem('price', price);
-    sessionStorage.setItem('stock', stock);
-    sessionStorage.setItem('image', imageUrl);
+function setSessionData(uuid) {
+    curr = inventory.find((p) => p._id == uuid);
 }
 
 document.getElementById('addToCart').addEventListener('click', () => {
@@ -119,42 +116,21 @@ document.getElementById('addToCart').addEventListener('click', () => {
 
     var quantityToAdd = document.querySelector("#addToCartInput").value;
     console.log("quantity: " + quantityToAdd);
-
-    let id = sessionStorage.getItem('id');
-    let name = sessionStorage.getItem('name');
-    let description = sessionStorage.getItem('description');
-    let price = sessionStorage.getItem('price');
-    let stock = sessionStorage.getItem('stock');
-    let image = sessionStorage.getItem('image');
-
-    console.log("id: " + id);
-    console.log("name: " + name);
-    console.log("description: " + description);
-    console.log("price: " + price);
-    console.log("stock: " + stock);
-    console.log("image: " + image);
+    console.log(curr);
 
     // Crear un nuevo objeto JSON para el producto actual
-    let producto = {
-        "id": id,
-        "name": name,
-        "description": description,
-        "price": price,
-        "stock": stock,
-        "image": image,
-        "quantity": quantityToAdd
-    };
+    curr["quantity"] = quantityToAdd;
 
     // Agregar el nuevo producto al carrito
-    carritoArr.push(producto);
+    carritoArr.push(curr);
 
     // Almacenar el carrito actualizado en el sessionStorage
     sessionStorage.setItem('carrito', JSON.stringify(carritoArr));
 
 });
 
-function focusProductsFunctions(id, name, description, price, stock, image){
-    setSessionData(id, name, description, price, stock, image);
+function focusProductsFunctions(uuid){
+    setSessionData(uuid);
     document.getElementById('buyProduct').showModal();
 }
 

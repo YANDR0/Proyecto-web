85ab7cc7a5665f3ead91efea97dbc6a4f3fc880f
ElCaffeesito <elcaffeesito777@gmail.com>
2023-11-29 13:56:08 -0600

    Revert "estudiante muere por no aguantar más de 7 desveladas"
    This reverts commit 2c460d97d74bc459a27a1fc0631c6e77e38060f0.

--- app/scripts/userProfile.js
+++ app/scripts/userProfile.js
@@ -214,17 +214,3 @@ document.getElementById('modifyU').addEventListener('click',() => {
     crudUserXHR(JSON.stringify(userJ), 'PUT');
 
 });
-document.querySelector('#ModifiedPhoneNum').addEventListener("input", () => {
-    setBubble(range, bubble);
-  });
-  
-function setBubble(range, bubble) {
-  const val = range.value;
-  const min = range.min ? range.min : 0;
-  const max = range.max ? range.max : 100;
-  const newVal = Number(((val - min) * 100) / (max - min));
-  bubble.innerHTML = val;
-
-  // Sorta magic numbers based on size of the native UI thumb
-  bubble.style.left = newVal = "%";
-}
